d21cfe1d9b07099e31cacb0f388a01ab37df1ed8
Manoj3109 <102951581+Manoj3109@users.noreply.github.com>
2023-10-30 10:41:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'; 
 import './App.css';
-import abhiImage from "./Screenshot-_51_.jpeg"; 
+import abhiImage from "Screenshot-_51_.jpeg"; 
 
 function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
